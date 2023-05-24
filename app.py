bd1f3df2fc5ad62250da6b875a19f8108476e02f
Ajith R B <ajithrb2001@gmail.com>
2023-05-24 16:21:52 +0530

    deploy

--- app.py
+++ app.py
@@ -128,4 +128,7 @@ def get_language_code(language):
 
 if __name__ == '__main__':
     nltk.download('punkt')
-    app.run(debug=True)
+    
+    from waitress import serve
+    serve(app, host='0.0.0.0', port=8000)
+
